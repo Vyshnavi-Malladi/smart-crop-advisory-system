5507294873c90b0ffa8188a1666117a1cbf24c15
MalladiVyshnavi <vyshnavimalladi2004@gmail.com>
2026-08-19 18:40:03 +0530

    Update frontend API URL for deployment

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -77,11 +77,66 @@
 
 
 
+// import axios from "axios";
+// import Cookies from "js-cookie";
+
+// const api = axios.create({
+//     baseURL: "http://localhost:5000/api",
+//     headers: {
+//         "Content-Type": "application/json"
+//     }
+// });
+
+// // ================= REQUEST INTERCEPTOR =================
+// api.interceptors.request.use(
+//     (config) => {
+
+//         const token = Cookies.get("token");
+
+//         if (token) {
+//             config.headers = config.headers || {};
+//             config.headers.Authorization = `Bearer ${token}`;
+//         }
+
+//         return config;
+//     },
+//     (error) => Promise.reject(error)
+// );
+
+// // ================= RESPONSE INTERCEPTOR =================
+// api.interceptors.response.use(
+//     (response) => response,
+//     (error) => {
+
+//         // If token expired or unauthorized → logout automatically
+//         if (error.response?.status === 401) {
+//             Cookies.remove("token");
+//             Cookies.remove("user");
+
+//             window.location.href = "/login";
+//         }
+
+//         return Promise.reject(error);
+//     }
+// );
+
+// export default api;
+
+
+
+
+
+
+
+
+
+
+
 import axios from "axios";
 import Cookies from "js-cookie";
 
 const api = axios.create({
-    baseURL: "http://localhost:5000/api",
+    baseURL: "https://farmxpert-backend.onrender.com/api",
     headers: {
         "Content-Type": "application/json"
     }
